f209f778ada06e536be158b0a76501cdf8bf1e5a
SherazKhan <sheraz@nmr.mgh.harvard.edu>
2019-10-04 09:49:06 -0400

    slider

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import nibabel
 import importlib
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -10,7 +9,6 @@ import mne
 from mne.minimum_norm import read_inverse_operator, apply_inverse
 import plotly.graph_objs as go
 from utils.helper_functions import mesh_edges, smoothing_matrix
-
 
 drc = importlib.import_module("utils.dash_reusable_components")
 figs = importlib.import_module("utils.figures")
@@ -23,12 +21,12 @@ app = dash.Dash(
 )
 server = app.server
 
-
-DEFAULT_COLORSCALE = [[0, 'rgb(12,51,131)'], [0.25, 'rgb(10,136,186)'],\
-        [0.5, 'rgb(242,211,56)'], [0.75, 'rgb(242,143,56)'], \
-        [1, 'rgb(217,30,30)']]
+DEFAULT_COLORSCALE = [[0, 'rgb(12,51,131)'], [0.25, 'rgb(10,136,186)'], \
+                      [0.5, 'rgb(242,211,56)'], [0.75, 'rgb(242,143,56)'], \
+                      [1, 'rgb(217,30,30)']]
 
 DEFAULT_COLORSCALE_NO_INDEX = [ea[1] for ea in DEFAULT_COLORSCALE]
+
 
 def plotly_triangular_mesh(vertices, faces, intensities=None, colorscale="Viridis",
                            flatshading=False, showscale=False, reversescale=False, plot_edges=False):
@@ -36,59 +34,60 @@ def plotly_triangular_mesh(vertices, faces, intensities=None, colorscale="Viridi
         faces = a numpy array of shape (n_faces, 3)
         intensities can be either a function of (x,y,z) or a list of values '''
 
-    x,y,z=vertices.T
-    I,J,K=faces.T
+    x, y, z = vertices.T
+    I, J, K = faces.T
 
-    mesh=dict(
+    mesh = dict(
         type='mesh3d',
         hoverinfo='none',
         x=x, y=y, z=z,
         colorscale=colorscale,
-        intensity= intensities,
+        intensity=intensities,
         flatshading=flatshading,
         i=I, j=J, k=K,
         name='',
         showscale=showscale
     )
 
-    mesh.update(lighting=dict( ambient= 0.18,
-                                  diffuse= 1,
-                                  fresnel=  0.1,
-                                  specular= 1,
-                                  roughness= 0.1,
-                                  facenormalsepsilon=1e-6,
-                                  vertexnormalsepsilon= 1e-12))
+    mesh.update(lighting=dict(ambient=0.18,
+                              diffuse=1,
+                              fresnel=0.1,
+                              specular=1,
+                              roughness=0.1,
+                              facenormalsepsilon=1e-6,
+                              vertexnormalsepsilon=1e-12))
 
     mesh.update(lightposition=dict(x=100,
-                                      y=200,
-                                      z= 0))
+                                   y=200,
+                                   z=0))
 
-    if  showscale is True:
-            mesh.update(colorbar=dict(thickness=20, ticklen=4, len=0.75))
+    if showscale is True:
+        mesh.update(colorbar=dict(thickness=20, ticklen=4, len=0.75))
 
-    if plot_edges is False: # the triangle sides are not plotted
-        return  [mesh]
-    else:#plot edges
-        #define the lists Xe, Ye, Ze, of x, y, resp z coordinates of edge end points for each triangle
-        #None separates data corresponding to two consecutive triangles
-        tri_vertices= vertices[faces]
-        Xe=[]
-        Ye=[]
-        Ze=[]
+    if plot_edges is False:  # the triangle sides are not plotted
+        return [mesh]
+    else:  # plot edges
+        # define the lists Xe, Ye, Ze, of x, y, resp z coordinates of edge end points for each triangle
+        # None separates data corresponding to two consecutive triangles
+        tri_vertices = vertices[faces]
+        Xe = []
+        Ye = []
+        Ze = []
         for T in tri_vertices:
-            Xe+=[T[k%3][0] for k in range(4)]+[ None]
-            Ye+=[T[k%3][1] for k in range(4)]+[ None]
-            Ze+=[T[k%3][2] for k in range(4)]+[ None]
-        #define the lines to be plotted
-        lines=dict(type='scatter3d',
-                   x=Xe,
-                   y=Ye,
-                   z=Ze,
-                   mode='lines',
-                   name='',
-                   line=dict(color= 'rgb(70,70,70)', width=1)
-               )
+            Xe += [T[k % 3][0] for k in range(4)] + [None]
+            Ye += [T[k % 3][1] for k in range(4)] + [None]
+            Ze += [T[k % 3][2] for k in range(4)] + [None]
+        # define the lines to be plotted
+        lines = dict(type='scatter3d',
+                     x=Xe,
+                     y=Ye,
+                     z=Ze,
+                     mode='lines',
+                     name='',
+                     line=dict(color='rgb(70,70,70)', width=1)
+                     )
         return [mesh, lines]
+
 
 data_path = mne.datasets.sample.data_path()
 
@@ -99,7 +98,7 @@ freesurfer_path = data_path + "/subjects/sample/surf/"
 
 lh = nibabel.freesurfer.io.read_geometry(freesurfer_path + "lh.inflated")[0]
 rh = nibabel.freesurfer.io.read_geometry(freesurfer_path + "rh.inflated")[0]
-rh[:,0] = rh[:,0] + 85
+rh[:, 0] = rh[:, 0] + 85
 
 snr = 3.0
 lambda2 = 1.0 / snr ** 2
@@ -108,10 +107,6 @@ inverse_operator = read_inverse_operator(fname_inv)
 evoked = mne.read_evokeds(fname_evoked, condition=0, baseline=(None, 0))
 
 src = inverse_operator['src']
-# lh_points = src[0]['rr'][src[0]['vertno'], :]
-# rh_points = src[1]['rr'][src[1]['vertno'], :]
-# points = np.r_[lh_points, rh_points]
-# points *= 170
 
 lh_points = src[0]['rr']
 rh_points = src[1]['rr']
@@ -127,23 +122,21 @@ use_faces = np.r_[src[0]['tris'], lh_points.shape[0] + src[1]['tris']]
 adj_mat = mesh_edges(use_faces)
 smooth_mat = smoothing_matrix(vertices, adj_mat)
 
-
-
 # Compute inverse solution
 pick_ori = "normal"  # Get signed values to see the effect of sign filp
 stc = apply_inverse(evoked, inverse_operator, lambda2, method,
                     pick_ori=pick_ori)
 index_time = np.abs(stc.data).mean(0).argmax()
 
-data=plotly_triangular_mesh(points, use_faces, smooth_mat*stc.data[:, index_time],
-                            colorscale=DEFAULT_COLORSCALE, flatshading=False,
-                            showscale=False, reversescale=False, plot_edges=False)
+data = plotly_triangular_mesh(points, use_faces, smooth_mat * stc.data[:, index_time],
+                              colorscale=DEFAULT_COLORSCALE, flatshading=False,
+                              showscale=False, reversescale=False, plot_edges=False)
 
 axis_template_time = dict(
-    showspikes = 'across+toaxis',
-    spikedash = 'solid',
-    spikemode  = 'across',
-    spikesnap = 'cursor',
+    showspikes='across+toaxis',
+    spikedash='solid',
+    spikemode='across',
+    spikesnap='cursor',
     showline=True,
     showgrid=True,
     zeroline=False,
@@ -153,7 +146,7 @@ axis_template_time = dict(
     zerolinecolor="white")
 
 axis_template = dict(
-    showspikes =None,
+    showspikes=None,
     title="",
     zeroline=False,
     showline=False,
@@ -166,42 +159,40 @@ axis_template = dict(
     zerolinecolor="rgb(0, 0, 0)")
 
 plot_layout = dict(
-         title = '',
-         margin=dict(t=0,b=0,l=0,r=0),
-         displayModeBar=False,
-         font=dict(size=12, color='white'),
-         width=650,
-         height=650,
-         showlegend=False,
-         plot_bgcolor='black',
-         paper_bgcolor='black',
-         scene=dict(xaxis=axis_template,
-                    yaxis=axis_template,
-                    zaxis=axis_template,
-                    aspectratio=dict(x=1, y=1.2, z=1),
-                    camera=dict(eye=dict(x=1.25, y=1.25, z=1.25)),
-                    annotations=[]
-                )
-        )
+    title='',
+    margin=dict(t=0, b=0, l=0, r=0),
+    displayModeBar=False,
+    font=dict(size=12, color='white'),
+    width=650,
+    height=650,
+    showlegend=False,
+    plot_bgcolor='black',
+    paper_bgcolor='black',
+    scene=dict(xaxis=axis_template,
+               yaxis=axis_template,
+               zaxis=axis_template,
+               aspectratio=dict(x=1, y=1.2, z=1),
+               camera=dict(eye=dict(x=1.25, y=1.25, z=1.25)),
+               annotations=[]
+               )
+)
 plot_layout_time = dict(
-         title = '',
-         font=dict(size=12, color='white'),
-         width=650,
-         height=650,
-         showlegend=False,
-         displayModeBar=False,
-         plot_bgcolor='black',
-         paper_bgcolor='black',
-         hovermode = 'closest',
-         xaxis = {'showspikes': True},
-         scene=dict(xaxis=axis_template_time,
-                    yaxis=axis_template_time,
-                    annotations=[]
-                )
+    title='',
+    font=dict(size=12, color='white'),
+    width=650,
+    height=650,
+    showlegend=False,
+    displayModeBar=False,
+    plot_bgcolor='black',
+    paper_bgcolor='black',
+    hovermode='closest',
+    xaxis={'showspikes': True},
+    scene=dict(xaxis=axis_template_time,
+               yaxis=axis_template_time,
+               annotations=[]
+               )
 
-        )
-
-
+)
 
 app.layout = html.Div(
     children=[
@@ -265,13 +256,13 @@ app.layout = html.Div(
                                         drc.NamedSlider(
                                             name="Time",
                                             id="slider-dataset-sample-size",
-                                            min=evoked.times.min()*1000,
-                                            max=evoked.times.max()*1000,
+                                            min=evoked.times.min() * 1000,
+                                            max=evoked.times.max() * 1000,
                                             step=len(evoked.times),
-                                            marks={ii: '{0:.0f}'.format(ii) if ii == evoked.times[0]*1000 else
-                                            '{0:.0f}'.format(ii) if not(i_l % 100) else ''
-                                                   for i_l, ii in enumerate(evoked.times*1000)},
-                                            value=int(len(evoked.times)/2),
+                                            marks={ii: '{0:.0f}'.format(ii) if ii == evoked.times[0] * 1000 else
+                                            '{0:.0f}'.format(ii) if not (i_l % 100) else ''
+                                                   for i_l, ii in enumerate(evoked.times * 1000)},
+                                            value=int(len(evoked.times) / 2),
                                         ),
                                         drc.NamedSlider(
                                             name="Threshold",
@@ -288,23 +279,22 @@ app.layout = html.Div(
                                     ],
                                 ),
                             ],
-                        ),html.Div([
-            dcc.Graph(id='g1', figure={
-                'data': [go.Scatter(
-            x=evoked.times*1000,
-            y=evoked.data[index, :].T,
-            mode='lines',
-            hoverinfo='x+y'
-        ) for index in np.arange(2, 306, 3)] + [go.Scatter(
-            x=[evoked.times[index_time]*1000, evoked.times[index_time]*1000],
-            y=[-np.abs(evoked.data[2:306:3]).max(), np.abs(evoked.data[2:306:3]).max()],
-            mode='lines',
-            line=dict(color='white', width=6),
-            hoverinfo='skip'
-        ) ],
-            'layout': plot_layout_time,})
-        ], className="six columns"),
-
+                        ), html.Div([
+                            dcc.Graph(id='g1', figure={
+                                'data': [go.Scatter(
+                                    x=evoked.times * 1000,
+                                    y=evoked.data[index, :].T,
+                                    mode='lines',
+                                    hoverinfo='x+y'
+                                ) for index in np.arange(2, 306, 3)] + [go.Scatter(
+                                    x=[evoked.times[index_time] * 1000, evoked.times[index_time] * 1000],
+                                    y=[-np.abs(evoked.data[2:306:3]).max(), np.abs(evoked.data[2:306:3]).max()],
+                                    mode='lines',
+                                    line=dict(color='white', width=6),
+                                    hoverinfo='skip'
+                                )],
+                                'layout': plot_layout_time, })
+                        ], className="six columns"),
 
                         html.Div(
                             [
@@ -332,29 +322,31 @@ app.layout = html.Div(
               [Input('slider-dataset-sample-size', 'value')],
               [State('brain-graph', 'figure')])
 def update_graph(selected_dropdown_value, figure):
-    index = (np.abs(stc.times*1000-selected_dropdown_value)).argmin()
-    data = plotly_triangular_mesh(points, use_faces, smooth_mat*stc.data[:, index],
+    index = (np.abs(stc.times * 1000 - selected_dropdown_value)).argmin()
+    data = plotly_triangular_mesh(points, use_faces, smooth_mat * stc.data[:, index],
                                   colorscale=DEFAULT_COLORSCALE, flatshading=False,
                                   showscale=False, reversescale=False, plot_edges=False)
     figure["data"] = data
     figure["layout"] = plot_layout
     return figure
 
+
 @app.callback(Output('g1', 'figure'),
               [Input('slider-dataset-sample-size', 'value')],
               [State('g1', 'figure')])
 def update_graph(selected_dropdown_value, figure):
-    index_time = (np.abs(stc.times*1000-selected_dropdown_value)).argmin()
+    index_time = (np.abs(stc.times * 1000 - selected_dropdown_value)).argmin()
 
     figure["data"][-1] = go.Scatter(
-            x=[evoked.times[index_time]*1000, evoked.times[index_time]*1000],
-            y=[-np.abs(evoked.data[2:306:3]).max(), np.abs(evoked.data[2:306:3]).max()],
-            mode='lines',
-            line=dict(color='white', width=6),
-            hoverinfo='skip'
-        )
+        x=[evoked.times[index_time] * 1000, evoked.times[index_time] * 1000],
+        y=[-np.abs(evoked.data[2:306:3]).max(), np.abs(evoked.data[2:306:3]).max()],
+        mode='lines',
+        line=dict(color='white', width=6),
+        hoverinfo='skip'
+    )
     figure["layout"] = plot_layout_time
     return figure
+
 
 # Running the server
 if __name__ == "__main__":
